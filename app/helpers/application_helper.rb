3a351f060bbca8c2434eecd4fa1a9f94d4f379a4
Mike Schwab <mike.schwab@gmail.com>
2008-12-03 00:47:24 -0500

    edit links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,11 @@ module ApplicationHelper
     link_to_function 'back', 'history.go(-1)'
   end
   
+  def edit_link obj
+    return unless obj.creator == ActiveRecord::Base.instance_variable_get('@current_user')
+    link_to 'edit', send("edit_#{obj.class.name.underscore}_path", obj)
+  end
+  
   def link_to_name obj
     return if obj.nil?
     text = obj.name.blank? ? obj.other_name : obj.name
