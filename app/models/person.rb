dc0045c8029f750e99f943b8a67f565197da3ef1
Mike Schwab <mike.schwab@gmail.com>
2008-12-04 16:53:26 -0500

    people have units

--- app/models/person.rb
+++ app/models/person.rb
@@ -6,6 +6,7 @@ class Person < ActiveRecord::Base
   has_many :emails, :through => :email_uses
   has_many :address_uses, :as => :target
   has_many :addresses, :through => :address_uses
+  has_many :units, :through => :address_uses
   has_many :website_uses, :as => :target
   has_many :websites, :through => :website_uses
   has_many :alias_uses, :as => :target
